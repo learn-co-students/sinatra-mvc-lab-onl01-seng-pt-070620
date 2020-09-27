db1e038a7316c710affee3c8e9fc0fff666ddb46
kinnari <kinnari1022@yahoo.com>
2020-09-26 21:38:27 -0400

    Done.

---
+++ models/pig_latinizer.rb
@@ -0,0 +1,27 @@
+class PigLatinizer
+
+
+    def piglatinize(word)
+        
+        first_letter = word[0].downcase
+        if ["a", "e", "i", "o", "u"].include?(first_letter)
+            "#{word}way"
+        else
+        consonants = []
+        consonants << word[0]
+            if ["a", "e", "i", "o", "u"].include?(word[1]) == false
+        consonants << word[1]
+            if ["a", "e", "i", "o", "u"].include?(word[2]) == false
+        consonants << word[2]
+        end
+      end
+    end
+    #binding.pry
+    #"#{word[consonants.length..-1] + consonants.join + "ay"}"
+  end
+
+
+
+
+
+end
