3a12602755ccf358e0b8467c1d034bea83fab41c
gracenak <gracenak@gmail.com>
2020-10-01 18:19:44 -0700

    Done.

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
         erb :user_input
     end
 
-    post '/piglatinize' do
+    post '/piglatinizer' do
         pl = PigLatinizer.new
         @piglatinized_text = pl.piglatinize(params[:user_phrase])
         
